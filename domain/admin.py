36ebc7b70a64117a7b158ce75ba45ac495f04efe
duanhongyi <duanhongyi@doopai.com>
2018-10-12 09:10:41 +0800

    fixbug

--- domain/admin.py
+++ domain/admin.py
@@ -10,17 +10,29 @@ from monitor.admin import MonitorInline
 class DomainAdmin(admin.ModelAdmin):
     list_display = ('name', 'dtype', 'created_time')
     search_fields = ('name', 'description')
-    autocomplete_fields = ('operators', )
-    exclude = ('user',)
     readonly_fields = []
 
+    def get_autocomplete_fields(self, request):
+        if request.user.is_superuser:
+            return ('operators', 'user')
+        return ('operators', )
+
+    def get_exclude(self, request, obj):
+        if request.user.is_superuser:
+            return ()
+        return ('user', )        
+
     def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
         return super().get_queryset(request).filter(user=request.user)
 
     def save_model(self, request, obj, form, change):
         if not obj.name.endswith('.'):
             obj.name = '%s.' % obj.name
-        obj.user = request.user
+        if not hasattr(obj, "user"):
+            obj.user = request.user
         super().save_model(request, obj, form, change)
 
 
@@ -37,15 +49,19 @@ class RecordAdmin(admin.ModelAdmin):
         'full_subdomain', 'region_name', 'rtype', 'content', 'status')
     search_fields = ('name', 'content', 'description')
     readonly_fields = ('full_subdomain', )
+    autocomplete_fields = ('domain', )
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "domain":
+        if db_field.name == "domain" and not request.user.is_superuser:
             q = Q(user=request.user) | Q(operators=request.user)
             kwargs["queryset"] = Domain.objects.filter(q)
         return super(RecordAdmin, self).formfield_for_foreignkey(
             db_field, request, **kwargs)
 
     def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
         q = Q(domain__user=request.user)
         q |= Q(domain__operators=request.user)
         return super().get_queryset(request).filter(q)
